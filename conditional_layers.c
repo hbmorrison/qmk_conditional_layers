8c8a87fb8279f9b27a5cad68a27be10aaa4fc79b
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2026-03-05 07:04:13 +0000

    Fixes to get conditional layers working
    commit 534a86f779f3af74d72ad6b0d5fa0225a5438c17
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Thu Mar 5 07:02:59 2026 +0000
    
        Simplify storage of conditional layer masks.
    
    commit 9586730679ea04061ab72af95029139366399416
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Tue Mar 3 23:09:55 2026 +0000
    
        Use layer_on and layer_off
    
    commit 09bdf288db63366a3e99a194e9bc7d50c71d0099
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Tue Mar 3 16:30:41 2026 +0000
    
        Rename init function and fix defines
    
    commit 3235357e7005ef2b818747d032186cc57887454d
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Tue Mar 3 00:27:19 2026 +0000
    
        Fix macro that creates a conditional layer struct
    
    commit 5f56726154ba3aba57b0bd7b84255a3db70ae6b5
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Mon Mar 2 20:40:30 2026 +0000
    
        Fix typos, add function declarations, fix struct
    
    commit 2d566cb172aaa68397e9b1d08f3e83998d4f22d5
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Mon Mar 2 20:39:55 2026 +0000
    
        Remove empty arrays/hashes
    
    commit 5618b7b87c31fcab952378bd4a1a94c9e8479822
    Author: Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
    Date:   Mon Mar 2 20:39:33 2026 +0000
    
        Fix typos

--- conditional_layers.c
+++ conditional_layers.c
@@ -12,166 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "print.h"
+#include "keymap.h"
 #include "conditional_layers.h"
 
-// Array that stores conditional layers that will be processed as the layer
-// state changes over time. By default all of the conditional layers in the
-// array are initialised to be 'off', and will not affect the layer state.
+// Stores conditional layer masks that will be checked against the layer state
+// as it changes over time.
 
-// Thank you to to https://github.com/yeroca for sharing this elegant way of
-// defining the size of an array-of-things in a static way that can also be
-// modified by the keymap writer (by defining MAX_CONDITIONAL_LAYER in
-// keymap.c).
+static layer_state_t conditional_layer[MAX_LAYER] = {
+  [0 ... MAX_LAYER - 1] = CL_NO_MASK
+};
 
-static conditional_layer_t conditional_layer[MAX_CONDITIONAL_LAYER] = {
-  [0 ... MAX_CONDITIONAL_LAYER - 1] = CONDITIONAL_LAYER(CL_CONDITIONAL_LAYER_OFF, CL_NO_LAYER)
-}
+layer_state_t layer_state_set_conditional_layers(layer_state_t state) {
 
-// This is a placeholder function that should be defined in keymap.c and should
-// contain a number of add_conditional_layer(CONDITIONAL_LAYER( ... )) function
-// calls to get all of the keymap's conditional layers defined up front.
+  // Go through every conditional layer.
 
-__attribute__ ((weak)) void conditional_layer_init () {
-  return;
-}
-
-// Call the conditional_layer_init() function after the keyboard initialisation
-// process has completed.
-
-void keyboard_post_init_conditional_layer () {
-  conditional_layer_init();
-}
-
-// Looks in the given layer state for any conditional layers that might apply. If one
-// is found, the layer state is modified before being returned. If more than one
-// is found, the layer comprising the highest combination of individual layers
-// will be selected. This also allows the use of an arithmetic comparison, which
-// is no coincidence.
-
-layer_state_t layer_state_set_conditional_layer(layer_state_t state) {
-  uint16_t      largest_index = 0;
-  layer_state_t largest_mask  = 0;
-
-  // Deactivate any currently active conditional layers whose masks no longer
-  // match the current layer state. The first part of the if statement is
-  // obvious. It excludes any conditional layers that are not active. The
-  // second part of the if statement relies on the logical-AND operator.
-  //
-  // If a conditional layer is currently active, then its layer mask must have
-  // matched the layer state, meaning that mask & state would have been at least
-  // identical to the layer state. If mask & state was not identical to state
-  // then state must have at least one more bit set to 1. Arithmetically, that
-  // means when they conditional layer is active, mask & state will be AT LEAST
-  // as large as mask.
-  //
-  // Now that the layer state has changed, if any of the layers that make up the
-  // same conditional layer mask are now not active, then mask & state will contain
-  // at least one zero that it never used to, which means that it MUST be
-  // smaller arithmetically than mask. Note that state itself may well be much
-  // larger, arithmetically speaking, since any number of layers not present in
-  // the mask may also be active - but the layer mask ANDs those potentially set
-  // bits out.
-
-  for (uint16_t index = 0; index < MAX_CONDITIONAL_LAYER; ++index) {
-    conditional_layer_t *cl = get_conditional_layer(index);
-    if (cl->active && cl->mask > cl->mask & state) {
-      cl->active = false;
-
-      // We can remove the now inactive conditional layer by ANDing the current
-      // layer state with the inverse of the layer (in other words, all 1s
-      // except for the layer bit itself, which will be 0).
-
-      state &= ~(cl->layer);
+  for (uint16_t layer = 0; layer < MAX_LAYER; ++layer) {
+    layer_state_t mask = conditional_layer[layer];
+    if (mask != CL_NO_MASK) {
+      if ((mask & state) == mask)
+        state |= LAYER_BIT(layer);
+      else
+        state &= ~(LAYER_BIT(layer));
     }
   }
-
-  // Now activate the conditional layer whose mask matches the current layer
-  // state. There may be more than one conditional layer mask that matches so
-  // this process involves finding the arithmetically largest conditional layer
-  // mask that matches the current layer state.
-  //
-  // Potentially ANY conditional layer whose mask matches could be activated,
-  // but right now the code selects the biggest one, which should correspond to
-  // the conditional layer that contains the highest currently active layer in
-  // its layer mask.
-
-  for (uint16_t index = 0; index < MAX_CONDITIONAL_LAYER; ++index) {
-
-    // Get the next conditional layer.
-
-    conditional_layer_t *cl = get_conditional_layer(index);
-
-    // If the cl mask matches the current layer state, save the mask and the
-    // current index, but only if the mask is larger than any mask that has
-    // already been found.
-
-    if (cl && state & cl->mask) {
-      if (cl->mask > largest_mask) {
-        largest_mask  = cl->mask;
-        largest_index = index;
-      }
-    }
-  }
-
-  // If a matching conditional layer was found, add the conditional layer bit to
-  // the current layer state.
-
-  if (largest_mask) {
-    conditional_layer_t *largest_cl = get_conditional_layer(largest_index);
-    state |= LAYER_BIT(largest_cl->layer);
-
-    // Set the conditional layer to active.
-
-    largest_cl->active = true;
-  }
-
-  // Return the potentially modified state.
-
   return state;
 }
 
-// Return a pointer to the conditional layer found at the given index.
-
-conditional_layer_t* get_conditional_layer(uint16_t index) {
-  return index < MAX_CONDITIONAL_LAYER ? &conditional_layer[index] : NULL;
+__attribute__((weak)) void conditional_layer_set() {
 }
 
-// Add the given conditional layer to the list of conditional layers if there is
-// room.
-
-uint16_t add_conditional_layer(conditional_layer_t *cl) {
-  for (uint16_t index = 0; index < MAX_CONDITIONAL_LAYER; ++index) {
-    if (conditional_layer[index].mask == CL_CONDITIONAL_LAYER_OFF) {
-
-      // Set the state of the conditional layer at the index to match the state
-      // of the argument.
-
-      conditional_layer[index].mask  = cl->mask;
-      conditional_layer[index].layer = cl->layer;
-
-      // Return the index of the conditional layer as a success code.
-
-      return index;
-    }
-  }
-
-  // If we reach here then no unused conditional layers has been found, so
-  // return an abberant conditional layer index.
-
-  return MAX_CONDITIONAL_LAYER + 1;
+void keyboard_post_init_conditional_layers() {
+  conditional_layer_set();
 }
 
-// Remove a conditional layer that matches the argument - although actually this
-// function will remove all conditional layers that match the argument.
-
-void remove_conditional_layer(conditional_layer_t *cl) {
-  for (uint16_t index = 0; index < MAX_CONDITIONAL_LAYER; ++index) {
-    if (conditional_layer[index].mask == cl->mask
-        && conditional_layer[index].layer == cl->layer) {
-
-      // Set the conditional layer at the index to the default state.
-
-      conditional_layer[index].mask  = CL_CONDITIONAL_LAYER_OFF;
-      conditional_layer[index].layer = CL_NO_LAYERS;
-    }
+void conditional_layer_set_mask(layer_state_t layer, layer_state_t mask) {
+  conditional_layer[layer] = mask;
 }
--- conditional_layers.h
+++ conditional_layers.h
@@ -16,69 +16,17 @@
 
 #include QMK_KEYBOARD_H
 
-// Set the default maximum number of conditional layers.
+// Indicates that the conditional layer is not being used.
 
-#ifndef MAX_CONDITIONAL_LAYER
-#define MAX_CONDITIONAL_LAYER MAX_LAYER
-#endif
+#define CL_NO_MASK (0)
 
-// Conditional layer.
-
-typedef struct conditional_layer_t {
-  layer_state_t mask;
-  layer_state_t layer;
-} conditional_layer_t;
-
-// This module makes the assumption that the base layer layer mask, which is
-// equivalent to 0, will not mean much since, almost always, the base layer will
-// be active, and also because conditional layers will usually be associated
-// with a layer-changing key press that introduces a higher layer. For this
-// reason, the zero'th layer can be used as a 'noop' flag that indicates the
-// conditional layer is not being used.
-
-#define CL_CONDITIONAL_LAYER_OFF 0
-#define CL_NO_LAYERS             (1 << 0)
-
-// List of layer bits that can be OR'ed to create the layer mask. It is easy to
-// write LAYER_ONE | LAYER_THREE but it might be less clear what those layers
-// are for, so also look at LAYER_BIT() below as a more semantic alternative.
-
-#define CL_LAYER_ONE             (1 << 1)
-#define CL_LAYER_TWO             (1 << 2)
-#define CL_LAYER_THREE           (1 << 3)
-#define CL_LAYER_FOUR            (1 << 4)
-#define CL_LAYER_FIVE            (1 << 5)
-#define CL_LAYER_SIX             (1 << 6)
-#define CL_LAYER_SEVEN           (1 << 7)
-#define CL_LAYER_EIGHT           (1 << 8)
-#define CL_LAYER_NINE            (1 << 9)
-#define CL_LAYER_TEN             (1 << 10)
-#define CL_LAYER_ELEVEN          (1 << 11)
-#define CL_LAYER_TWELVE          (1 << 12)
-#define CL_LAYER_THIRTEEN        (1 << 13)
-#define CL_LAYER_FOURTEEN        (1 << 14)
-#define CL_LAYER_FIFTEEN         (1 << 15)
-
-// Layer masks can also be constructed by using LAYER_BIT() to work out the bit
-// of the layer mask for a given layer. The layer mask can be built up by ORing
-// together LAYER_BIT() statements.
-//
-// For example, LAYER_BIT(LAYER_NUM) | LAYER_BIT(LAYER_SYMBOL) defines a
-// two-layer mask, where LAYER_NUM and LAYER_SYMBOL are layers defined in the
-// layer enum in keymap.c.
+// Works out the layer bit associated with a given layer. A layer mask can be
+// built up using logical-or.
+// e.g. LAYER_BIT(LAYER_BASE) | LAYER_BIT(LAYER_SYMBOL)
 
 #define LAYER_BIT(code) (1 << ((code) & 0x7f))
 
-// Define a conditional layer.
-
-#define CONDITIONAL_LAYER(mask, layer) { \
-  .mask   = (mask),                      \
-  .layer  = (layer),                     \
-  .active = false                        \
-}
-
 // Function declarations.
 
-conditional_layer_t* get_conditional_layer(uint16_t);
-uint16_t add_conditional_layer(conditional_layer_t *);
-void remove_conditional_layer(conditional_layer_t *);
+void conditional_layer_set(void);
+void conditional_layer_set_mask(layer_state_t, layer_state_t);
